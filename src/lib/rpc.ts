f1c87786c75174ed25fe3f4d9cc059ca41c6c9a8
sptres <shawnpark2397@gmail.com>
2024-11-23 15:21:12 -0800

    fixed log in issue

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -2,4 +2,6 @@ import { hc } from 'hono/client';
 
 import { AppType } from '@/app/api/[[...route]]/route';
 
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
+const BASE_URL = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
+
+export const client = hc<AppType>(BASE_URL);
